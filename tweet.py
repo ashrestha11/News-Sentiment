97c98208b71648afd6e922b2e2cfac07cd68d298
ashretha11 <shrestha.anurag7@gmail.com>
2020-10-29 14:12:34 -0700

    - streams

--- tweet.py
+++ tweet.py
@@ -8,16 +8,10 @@ import json
 # import api keys
 api_key, api_secret_key, access_token, access_token_s = keys.api_keys()
 
-# Oauth
-auth = OAuthHandler(api_key, api_secret_key)
-auth.set_access_token(access_token, access_token_s)
-api = API(auth, wait_on_rate_limit=True,
-          wait_on_rate_limit_notify=True)
-
 
 def analyze_status(text):
 
-    if '@' not in text[0]:
+    if '@' not in text[0] or 'RT' not in text[0:2]:
         print(text)
     else:
         pass
@@ -25,17 +19,37 @@ def analyze_status(text):
 
 class MyStreamListener(StreamListener):
 
+    def on_data(self, raw_data):
+        # load the json and filter it
+        pass
+
     def on_status(self, status):
 
-        if not hasattr(status, "retweeted_status"):
-            try:
-                print(analyze_status(status.extended_tweet["full_text"]))
+        try:
+            print(analyze_status(status.text))
 
-            except AttributeError:
-                print(status.text)
+        except AttributeError:
+            print(analyze_status(status.text))
 
 
-myStreamListener = MyStreamListener()
-myStream = Stream(auth=api.auth, listener=myStreamListener)
+class TweepyStream():
 
-myStream.filter(follow=['2704294333', '624413', '15110357'])
+    def __init__(self, auth, listener):
+        self.stream = Stream(auth=auth, listener=listener)
+
+    def start(self, tweet_id):
+        self.stream.filter(follow=tweet_id)
+
+
+if __name__ == '__main__':
+
+    myStreamListener = MyStreamListener()
+
+    auth = OAuthHandler(api_key, api_secret_key)
+    auth.set_access_token(access_token, access_token_s)
+    api = API(auth, wait_on_rate_limit=True,
+              wait_on_rate_limit_notify=True)
+
+    stream = TweepyStream(auth=api.auth, listener=myStreamListener)
+    stream.start(['2704294333', '624413', '15110357','988955288'])
+
